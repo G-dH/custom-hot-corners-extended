3a3ba3b0a8e7cc6689b1785e7ac4980cb66d68a5
Jan Runge <janrunx@gmail.com>
2020-02-10 22:10:41 +0100

    Change gsettings schema
    Every corner will now have it's own schema path.

--- extension.js
+++ extension.js
@@ -26,11 +26,10 @@ const Ripples = imports.ui.ripples;
 const Util = imports.misc.util;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
-const Convenience = Me.imports.convenience;
+const Settings = Me.imports.settings;
 
-let _settings = Convenience.getSettings();
-let _updateHotCornersId = null;
 let _origUpdateHotCorners = Main.layoutManager._updateHotCorners;
+let _monitors = [];
 
 function init() {
 }
@@ -38,14 +37,9 @@ function init() {
 function enable() {
     Main.layoutManager._updateHotCorners = _updateHotCorners;
     Main.layoutManager._updateHotCorners();
-    _updateHotCornersId = _settings.connect(
-        'changed::actions',
-        Main.layoutManager._updateHotCorners
-    );
 }
 
 function disable() {
-    _settings.disconnect(_updateHotCornersId);
     // This restores the original hot corners
     _removeHotCorners();
     Main.layoutManager._updateHotCorners = _origUpdateHotCorners;
@@ -59,27 +53,28 @@ function _removeHotCorners() {
 }
 
 function _updateHotCorners() {
+    _monitors.forEach(m => m.destroy());
+    _monitors = [];
     _removeHotCorners();
-    let actions = _settings.get_value('actions').deep_unpack();
-    for (let [monitorNumber, top, left, id, cmd] of actions) {
-        let monitor = Main.layoutManager.monitors[monitorNumber];
-        if (monitor && id !== 'disabled') {
-            let c = new CustomHotCorner(monitor, top, left, id, cmd);
-            c.setBarrierSize(20);
-            Main.layoutManager.hotCorners.push(c);
+    _monitors = Settings.Monitor.all();
+
+    for (let monitor of _monitors) {
+        for (let corner of monitor.corners) {
+            // Update all hot corners if something changes
+            corner.connect('changed', () => _updateHotCorners());
+            if (corner.action !== 'disabled') {
+                Main.layoutManager.hotCorners.push(new CustomHotCorner(corner));
+            }
         }
     }
 }
 
 const CustomHotCorner = class CustomHotCorner extends Layout.HotCorner {
-    constructor(monitor, top, left, action, command) {
-        let x = left ? monitor.x : monitor.x + monitor.width;
-        let y = top ? monitor.y : monitor.y + monitor.height;
-        super(Main.layoutManager, monitor, x, y);
-        this._top = top;
-        this._left = left;
-        this._action = action;
-        this._command = command;
+    constructor(corner) {
+        let monitor = Main.layoutManager.monitors[corner.monitorIndex];
+        super(Main.layoutManager, monitor, corner.x, corner.y);
+        this._corner = corner;
+        this._monitor = monitor;
 
         let m = new Map([
             ['toggleOverview', this._toggleOverview],
@@ -87,26 +82,22 @@ const CustomHotCorner = class CustomHotCorner extends Layout.HotCorner {
             ['showApplications', this._showApplications],
             ['runCommand', this._runCommand]
         ]);
-        this._actionFunction = m.get(action) || function () {};
-
-        this._x = x;
-        this._y = y;
+        this._actionFunction = m.get(this._corner.action) || function () {};
 
         // Avoid pointer barriers that are at the same position
         // but block opposite directions. Neither with X nor with Wayland
         // such barriers work.
         for (let c of Main.layoutManager.hotCorners) {
-            if (this._x === c._x && this._y === c._y) {
-                if (this._top === c._top) {
-                    this._x += this._left ? 1 : -1;
-                } else if (this._left === c._left) {
-                    this._y += this._top ? 1 : -1;
+            if (this._corner.x === c._x && this._corner.y === c._y) {
+                if (this._corner.top === c._top) {
+                    this._corner.x += this._corner.left ? 1 : -1;
+                } else if (this._corner.left === c._left) {
+                    this._corner.y += this._corner.top ? 1 : -1;
                 }
             }
         }
 
         this._enterd = false;
-        this._monitor = monitor;
         this._pressureBarrier = new Layout.PressureBarrier(
             Layout.HOT_CORNER_PRESSURE_THRESHOLD,
             Layout.HOT_CORNER_PRESSURE_TIMEOUT,
@@ -114,16 +105,17 @@ const CustomHotCorner = class CustomHotCorner extends Layout.HotCorner {
         );
 
         this._pressureBarrier.connect('trigger', this._runAction.bind(this));
-
         this._setupFallbackCornerIfNeeded(Main.layoutManager);
 
         // Rotate the ripple actors according to the corner.
         let ltr = (Clutter.get_default_text_direction() ==
                    Clutter.TextDirection.LTR);
-        let angle = (left && ltr) ? (top ? 0 : 270) : (top ? 90 : 180);
+        let angle = (this._corner.left && ltr) ? (this._corner.top ? 0 : 270) : (this._corner.top ? 90 : 180);
         this._ripples._ripple1.rotation_angle_z = angle;
         this._ripples._ripple2.rotation_angle_z = angle;
         this._ripples._ripple3.rotation_angle_z = angle;
+
+        this.setBarrierSize(20);
     }
 
     // Overridden to allow all 4 monitor corners
@@ -136,19 +128,19 @@ const CustomHotCorner = class CustomHotCorner extends Layout.HotCorner {
             const BD = Meta.BarrierDirection;
             this._verticalBarrier = new Meta.Barrier({
                 display: global.display,
-                x1: this._x,
-                x2: this._x,
-                y1: this._y,
-                y2: this._top ? this._y + size : this._y - size,
-                directions: this._left ? BD.POSITIVE_X : BD.NEGATIVE_X
+                x1: this._corner.x,
+                x2: this._corner.x,
+                y1: this._corner.y,
+                y2: this._corner.top ? this._corner.y + size : this._corner.y - size,
+                directions: this._corner.left ? BD.POSITIVE_X : BD.NEGATIVE_X
             });
             this._horizontalBarrier = new Meta.Barrier({
                 display: global.display,
-                x1: this._x,
-                x2: this._left ? this._x + size : this._x - size,
-                y1: this._y,
-                y2: this._y,
-                directions: this._top ? BD.POSITIVE_Y : BD.NEGATIVE_Y
+                x1: this._corner.x,
+                x2: this._corner.left ? this._corner.x + size : this._corner.x - size,
+                y1: this._corner.y,
+                y2: this._corner.y,
+                directions: this._corner.top ? BD.POSITIVE_Y : BD.NEGATIVE_Y
             });
 
             this._pressureBarrier.addBarrier(this._verticalBarrier);
@@ -163,31 +155,31 @@ const CustomHotCorner = class CustomHotCorner extends Layout.HotCorner {
 
         this.actor = new Clutter.Actor({
             name: 'hot-corner-environs',
-            x: this._x, y: this._y,
+            x: this._corner.x, y: this._corner.y,
             width: 3, height: 3,
             reactive: true,
-            scale_x: this._left ? 1 : -1,
-            scale_y: this._top ? 1 : -1
+            scale_x: this._corner.left ? 1 : -1,
+            scale_y: this._corner.top ? 1 : -1
         });
 
-        this._corner = new Clutter.Actor({
+        this._cornerActor = new Clutter.Actor({
             name: 'hot-corner',
             x: 0, y: 0,
             width: 1, height: 1,
             reactive: true
         });
 
-        this._corner._delegate = this;
-        this.actor.add_child(this._corner);
+        this._cornerActor._delegate = this;
+        this.actor.add_child(this._cornerActor);
         layoutManager.addChrome(this.actor);
 
         this.actor.connect('leave-event', this._onEnvironsLeft.bind(this));
-        this._corner.connect('enter-event', this._onCornerEntered.bind(this));
-        this._corner.connect('leave-event', this._onCornerLeft.bind(this));
+        this._cornerActor.connect('enter-event', this._onCornerEntered.bind(this));
+        this._cornerActor.connect('leave-event', this._onCornerLeft.bind(this));
     }
 
     _rippleAnimation() {
-        this._ripples.playAnimation(this._x, this._y);
+        this._ripples.playAnimation(this._corner.x, this._corner.y);
     }
 
     // Overridden to allow running custom actions
@@ -229,6 +221,6 @@ const CustomHotCorner = class CustomHotCorner extends Layout.HotCorner {
 
     _runCommand() {
         this._rippleAnimation();
-        Util.spawnCommandLine(this._command);
+        Util.spawnCommandLine(this._corner.command);
     }
 }
