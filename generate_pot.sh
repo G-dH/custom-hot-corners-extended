48dd32e92f13959042f4519a7993496711998470
G-dH <georgdh@gmail.com>
2022-04-10 11:47:22 +0200

    files cleanup

--- generate_pot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#find ./ -name '*.ui' -exec intltool-extract --type=gettext/glade {} \;
-#find ./ -regex '\(.*\.js\|.*\.h\)' -exec xgettext --keyword=_ --keyword=N_ --output=custom-hot-corners-extended.pot {} \+
-find ./ -regex '.*\.js' -exec xgettext --keyword=_ --output=custom-hot-corners-extended.pot {} \+
-
-#rm -f *.h
