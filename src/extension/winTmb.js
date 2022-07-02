28e0061937d379f815cf94b894513a9f2551738a
G-dH <georgdh@gmail.com>
2022-07-02 02:53:57 +0200

    winTmb: fix: resize, add: hover preview, rm: app icon

--- src/extension/winTmb.js
+++ src/extension/winTmb.js
@@ -19,15 +19,20 @@
 
 const { GObject, GLib, Clutter, St, Meta, Shell } = imports.gi;
 
-const Main                   = imports.ui.main;
-const DND                    = imports.ui.dnd;
-const AltTab                 = imports.ui.altTab;
+const Main         = imports.ui.main;
+const DND          = imports.ui.dnd;
+const AltTab       = imports.ui.altTab;
+const Graphene     = imports.gi.Graphene;
+const shellVersion = parseFloat(imports.misc.config.PACKAGE_VERSION);
 
 const SCROLL_ICON_OPACITY = 240;
+const DRAG_OPACITY = 200;
+const CLOSE_BTN_OPACITY = 240;
 
 var   WindowThumbnail = GObject.registerClass(
 class WindowThumbnail extends St.BoxLayout {
     _init(metaWin, parent, args) {
+        this._hoverShowsPreview = false;
         this._initTmbHeight = args.height;
         this._minimumHeight = Math.floor(5 / 100 * global.display.get_monitor_geometry(global.display.get_current_monitor()).height);
         this._actionTimeoutId = null;
@@ -40,15 +45,18 @@ class WindowThumbnail extends St.BoxLayout {
         this.w = metaWin;
         super._init({visible: true, reactive: true, can_focus: true, track_hover: true});
         this.connect('button-release-event', this._onBtnReleased.bind(this));
-        this.connect('button-press-event', this._onBtnPressed.bind(this));
         this.connect('scroll-event', this._onScrollEvent.bind(this));
-        // this.connect('motion-event', this._onMouseMove.bind(this)); // may be useful in the future..
+        //this.connect('motion-event', this._onMouseMove.bind(this)); // may be useful in the future..
 
         this._delegate = this;
-        this._draggable = DND.makeDraggable(this, {dragActorOpacity: 200});
-        this.saved_snap_back_animation_time = DND.SNAP_BACK_ANIMATION_TIME;
+        this._draggable = DND.makeDraggable(this, {dragActorOpacity: DRAG_OPACITY});
         this._draggable.connect('drag-end', this._end_drag.bind(this));
         this._draggable.connect('drag-cancelled', this._end_drag.bind(this));
+        this._draggable._animateDragEnd = (eventTime) => {
+            this._draggable._animationInProgress = true;
+            this._draggable._onAnimationComplete(this._draggable._dragActor, eventTime);
+            this.opacity = 255;
+        };
 
         this.clone = new Clutter.Clone({reactive: true});
         Main.layoutManager.addChrome(this);
@@ -58,9 +66,8 @@ class WindowThumbnail extends St.BoxLayout {
         this.clone.set_source(this.window);
 
         this._tmb = new St.Widget({layout_manager: new Clutter.BinLayout()});
-        //this._tmb.set_style('border-color: #353535; border: 2px; border-radius: 4px');
         this.add_child(this._tmb);
-        //this.set_child(this.clone);
+
         this._bin = new St.Bin();
         this._bin.set_child(this.clone);
         this._tmb.add_child(this._bin);
@@ -68,11 +75,14 @@ class WindowThumbnail extends St.BoxLayout {
         this._addScrollModeIcon();
 
         this.connect('enter-event', () => {
-            if (this._bin.get_child() === this.icon)
-                return;
-            this._closeButton.opacity = 255;
+            this._closeButton.opacity = CLOSE_BTN_OPACITY;
             this._scrollModeBin.opacity = SCROLL_ICON_OPACITY;
+            if (this._hoverShowsPreview) {
+                this._closeButton.opacity = 50;
+                this._showWindowPreview();
+            }
         });
+
         this.connect('leave-event', () => {
             this._closeButton.opacity = 0;
             this._scrollModeBin.opacity = 0;
@@ -92,12 +102,10 @@ class WindowThumbnail extends St.BoxLayout {
             if (this)
                 this._remove();
         });
-        this._setIcon();
+        //this._setIcon();
     }
 
     _getInitialPosition() {
-        // let pointer = {};
-        // [pointer.x, pointer.y,] = global.get_pointer();
         const offset = 20;
         let monitor = Main.layoutManager.monitors[global.display.get_current_monitor()];
         let x = Math.min(monitor.x + monitor.width  - (this.window.width  * this.scale) - offset);
@@ -107,33 +115,33 @@ class WindowThumbnail extends St.BoxLayout {
 
     _setSize(resetScale = false) {
         if (resetScale)
-            // this.scale = Math.min(1.0, this.max_width / this.window.width, this.max_height / this.window.height);
             this.scale = Math.min(1.0, this._initTmbHeight / this.window.height);
-        // when this.clone source window resize, this.clone and this. actor resize accordingly
-        //this.scale_x = this.scale;
-        //this.scale_y = this.scale;
-        this._bin.width = this.window.width * this.scale;
-        this._bin.height = this.window.height * this.scale;
+
+        const width = this.window.width * this.scale;
+        const height = this.window.height * this.scale;
+        this._bin.width = width;
+        this._bin.height = height;
+        this.width = width;
+        this.height = height;
         if (this.icon) {
-            //this.icon.scale_x = this.scale;
-            //this.icon.scale_y = this.scale;
+            this.icon.scale_x = this.scale;
+            this.icon.scale_y = this.scale;
         }
-        //this._closeButton.scale_x = 1 / this.scale;
-        //this._closeButton.scale_y = 1 / this.scale;
+
         // when the scale of this. actor change, this.clone resize accordingly,
         // but the reactive area of the actor doesn't change until the actor is redrawn
         // this updates the actor's input region area:
         Main.layoutManager._queueUpdateRegions();
     }
 
-    _onMouseMove(actor, event) {
+    /*_onMouseMove(actor, event) {
         let [pos_x, pos_y] = event.get_coords();
         let state = event.get_state();
         if (this._ctrlPressed(state)) {
         }
-    }
+    }*/
 
-    _onBtnPressed(actor, event) {
+    _onBtnReleased(actor, event) {
         // Clutter.Event.click_count property in no longer available, since GS42
         //let doubleclick = event.get_click_count() === 2;
         if ((event.get_time() - this._prevBtnPressTime) < Clutter.Settings.get_default().double_click_time) {
@@ -146,10 +154,7 @@ class WindowThumbnail extends St.BoxLayout {
         if (this._click_count === 2 && event.get_button() === Clutter.BUTTON_PRIMARY) {
             this.w.activate(global.get_current_time());
         }
-        return Clutter.EVENT_PROPAGATE;
-    }
 
-    _onBtnReleased(actor, event) {
         const button = event.get_button();
         const state = event.get_state();
         switch (button) {
@@ -163,9 +168,12 @@ class WindowThumbnail extends St.BoxLayout {
             }
             return Clutter.EVENT_STOP;
         case Clutter.BUTTON_SECONDARY:
-            // if (_ctrlPressed(state))
-            //this._remove();
-            this._showWindowPreview();
+            if (this._ctrlPressed(state)) {
+                this._remove();
+            } else {
+                this._hoverShowsPreview = !this._hoverShowsPreview;
+                this._showWindowPreview();
+            }
             return Clutter.EVENT_STOP;
         case Clutter.BUTTON_MIDDLE:
             if (this._ctrlPressed(state))
@@ -206,7 +214,6 @@ class WindowThumbnail extends St.BoxLayout {
             return Clutter.EVENT_PROPAGATE;
         }
         this._setSize();
-        // this.scale = Math.min(1.0, this.max_width / this.width, this.max_height / this.height);
         return Clutter.EVENT_STOP;
     }
 
@@ -224,10 +231,7 @@ class WindowThumbnail extends St.BoxLayout {
 
     _end_drag() {
         this.set_position(this._draggable._dragOffsetX + this._draggable._dragX, this._draggable._dragOffsetY + this._draggable._dragY);
-        DND.SNAP_BACK_ANIMATION_TIME = 0;
-        this.timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 0, () => {
-            DND.SNAP_BACK_ANIMATION_TIME = this.saved_snap_back_animation_time;
-        });
+        this._setSize();
     }
 
     _ctrlPressed(state) {
@@ -265,7 +269,7 @@ class WindowThumbnail extends St.BoxLayout {
         });
         this.w = w;
 
-        this._setIcon();
+        //this._setIcon();
         if (this._winPreview) {
             this._showWindowPreview(true);
         }
@@ -277,7 +281,7 @@ class WindowThumbnail extends St.BoxLayout {
         this._actionTimeoutId = GLib.timeout_add(
             GLib.PRIORITY_DEFAULT,
             // timeout for resizing should be shorter than for window switching
-            this._reverseTmbWheelFunc ? this._scrollTimeout : this._scrollTimeout / 2,
+            this._reverseTmbWheelFunc ? this._scrollTimeout : this._scrollTimeout / 4,
             this._removeActionTimeout.bind(this)
         );
         return false;
@@ -296,7 +300,7 @@ class WindowThumbnail extends St.BoxLayout {
         let icon = app
             ? app.create_icon_texture(this.height)
             : new St.Icon({icon_name: 'icon-missing', icon_size: this.height});
-        icon.x_expand = icon.y_expand = false;
+        icon.x_expand = icon.y_expand = true;
         if (this.icon)
             this.icon.destroy();
         this.icon = icon;
@@ -305,34 +309,26 @@ class WindowThumbnail extends St.BoxLayout {
     _switchView(clone = false) {
         if (clone) {
             this._bin.set_child(this.clone);
-        } else {
-            this._bin.set_child(
-                this._bin.get_child() === this.clone
-                ? this.icon
-                : this.clone
-            );
         }
     }
 
     _addCloseButton() {
-        const closeButton = new St.Icon({
+        const closeButton = new St.Button({
+            opacity: 0,
             style_class: 'window-close',
-            icon_name: 'window-close-symbolic',
+            child: new St.Icon({ icon_name: shellVersion < 40 ? 'window-close-symbolic' : 'preview-close-symbolic' }),
             x_align: Clutter.ActorAlign.END,
             y_align: Clutter.ActorAlign.START,
             x_expand: true,
             y_expand: true,
-            reactive: true,
         });
-        closeButton.set_style('/*background-color: dimgrey;*/ width: 1.3em; height: 1.3em; padding: 2px;');
-        closeButton.connect('button-press-event', () => { return Clutter.EVENT_STOP; });
-        closeButton.connect('button-release-event', () => {
+
+        closeButton.connect('clicked', () => {
             this._remove();
             return Clutter.EVENT_STOP;
         });
 
         this._closeButton = closeButton;
-        this._closeButton.opacity = 0;
         this._tmb.add_child(this._closeButton);
     }
 
@@ -344,18 +340,24 @@ class WindowThumbnail extends St.BoxLayout {
         this._scrollModeResizeIcon = new St.Icon({
             icon_name: 'view-fullscreen-symbolic',
             x_align: Clutter.ActorAlign.CENTER,
-            y_align: Clutter.ActorAlign.CENTER,
+            y_align: Clutter.ActorAlign.END,
             x_expand: true,
             y_expand: true,
-            opacity: SCROLL_ICON_OPACITY
+            opacity: SCROLL_ICON_OPACITY,
+            style_class: 'icon-dropshadow',
+            scale_x: 0.5,
+            scale_y: 0.5
         });
         this._scrollModeSourceIcon = new St.Icon({
-            icon_name: 'window-new-symbolic',
+            icon_name: 'media-skip-forward-symbolic',
             x_align: Clutter.ActorAlign.CENTER,
-            y_align: Clutter.ActorAlign.CENTER,
+            y_align: Clutter.ActorAlign.END,
             x_expand: true,
             y_expand: true,
-            opacity: SCROLL_ICON_OPACITY
+            opacity: SCROLL_ICON_OPACITY,
+            style_class: 'icon-dropshadow',
+            scale_x: 0.5,
+            scale_y: 0.5
         });
         this._scrollModeBin.set_child(this._scrollModeResizeIcon);
         this._tmb.add_child(this._scrollModeBin);
@@ -363,8 +365,13 @@ class WindowThumbnail extends St.BoxLayout {
     }
 
     _showWindowPreview(update = false) {
+        // filter out double events
+        if (Date.now() - this._previewCreationTime < 100) return;
+
         if (this._winPreview) {
             this._destroyWindowPreview();
+            this._previewCreationTime = 0;
+            this._closeButton.opacity = CLOSE_BTN_OPACITY;
             if (!update)
                 return;
         }
@@ -373,6 +380,7 @@ class WindowThumbnail extends St.BoxLayout {
             this._winPreview = new AltTab.CyclerHighlight();
             global.window_group.add_actor(this._winPreview);
             [this._winPreview._xPointer, this._winPreview._yPointer] = global.get_pointer();
+            this._previewCreationTime = Date.now();
         }
 
         if (!update) {
@@ -382,11 +390,12 @@ class WindowThumbnail extends St.BoxLayout {
                 duration: 70,
                 mode: Clutter.AnimationMode.LINEAR,
                 onComplete: () => {
+                    this._closeButton.opacity = 50;
                 },
             });
 
             this.ease({
-                opacity: 50,
+                opacity: 150,
                 duration: 70,
                 mode: Clutter.AnimationMode.LINEAR,
                 onComplete: () => {
