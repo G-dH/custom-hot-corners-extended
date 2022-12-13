7a8a999374707573281d6ae82b8dc667bb793ff9
G-dH <georgdh@gmail.com>
2022-12-13 02:07:48 +0100

    improved action: protanopia correction filter

--- src/extension/shaders.js
+++ src/extension/shaders.js
@@ -122,62 +122,69 @@ var ShaderLib = class {
             #define SIMULATE ${simulate}
             void main() {
                 vec4 c = texture2D(tex, cogl_tex_coord_in[0].st);
-            // RGB to LMS matrix
+
+                // RGB to LMS matrix
                 float L = (17.8824f * c.r) + (43.5161f * c.g) + (4.11935f * c.b);
                 float M = (3.45565f * c.r) + (27.1554f * c.g) + (3.86714f * c.b);
                 float S = (0.0299566f * c.r) + (0.184309f * c.g) + (1.46709f * c.b);
-            // Simulate color blindness
-                #if ( COLORBLIND_MODE == 1) // Protanope - reds are greatly reduced (1% men)
+
+                // Remove invisible colors
+                #if ( COLORBLIND_MODE == 1) // Protanopia - reds are greatly reduced
                     float l = 0.0f * L + 2.02344f * M + -2.52581f * S;
                     float m = 0.0f * L + 1.0f * M + 0.0f * S;
                     float s = 0.0f * L + 0.0f * M + 1.0f * S;
                 #endif
-                #if ( COLORBLIND_MODE == 2) // Deuteranope - greens are greatly reduced (1% men)
+                #if ( COLORBLIND_MODE == 2) // Deuteranopia - greens are greatly reduced
                     float l = 1.0f * L + 0.0f * M + 0.0f * S;
                     float m = 0.494207f * L + 0.0f * M + 1.24827f * S;
                     float s = 0.0f * L + 0.0f * M + 1.0f * S;
                 #endif
-                #if ( COLORBLIND_MODE == 3) // Tritanope - blues are greatly reduced (0.003% population)
+                #if ( COLORBLIND_MODE == 3) // Tritanopia - blues are greatly reduced (1 of 10 000)
                     float l = 1.0f * L + 0.0f * M + 0.0f * S;
                     float m = 0.0f * L + 1.0f * M + 0.0f * S;
-                    //float s = -0.237454f * L + 1.237458f * M + 0.0f * S;
-                    // GdH - This vector is calculated by me
+                    // GdH - trinatopia vector calculated by me, all public sources were off
                     float s = -0.012491378299329402f * L + 0.07203451899279534f * M + 0.0f * S;
                 #endif
-            // LMS to RGB matrix conversion
+
+                // LMS to RGB matrix conversion
                 vec4 error;
                 error.r = (0.0809444479f * l) + (-0.130504409f * m) + (0.116721066f * s);
                 error.g = (-0.0102485335f * l) + (0.0540193266f * m) + (-0.113614708f * s);
                 error.b = (-0.000365296938f * l) + (-0.00412161469f * m) + (0.693511405f * s);
                 error.a = 1;
 
-            // The error is what they see
+                // The error is what they see
                 #if (SIMULATE == 1)
                     error.a = c.a;
                     cogl_color_out = error.rgba;
                     return;
                 #endif
                 #if (SIMULATE == 0)
-            // Isolate invisible colors to color vision deficiency (calculate error matrix)
+                    // Isolate invisible colors to color vision deficiency (calculate error matrix)
                     error = (c - error);
-            // Shift colors similar to Android colorblind filters
+                    
+                    // Shift colors
                     vec4 correction;
-                #if ( COLORBLIND_MODE == 1 )
-                    correction.r = -error.r/4 - error.g;
-                    correction.g = -error.g;
-                    correction.b = (error.r/3 + error.b);
+                    // protanopia / protanomaly corrections (kwin effect values)
+                    #if ( COLORBLIND_MODE == 1 )
+                        correction.r = error.r * 0.56667 + error.g * 0.43333 + error.b * 0.00000;
+                        correction.g = error.r * 0.55833 + error.g * 0.44267 + error.b * 0.00000;
+                        correction.b = error.r * 0.00000 + error.g * 0.24167 + error.b * 0.75833;
 
-                #elif ( COLORBLIND_MODE == 2 )
-                // this one needs more work
-                    correction.r = -0.7 * error.r;
-                    correction.g = error.r/2 + error.g;
-                    correction.b = -error.r/3 + error.b;
-                #elif ( COLORBLIND_MODE == 3 )
-                    correction.r = (error.b + error.r) * 0.3;
-                    correction.g = (error.b + error.g) * 0.3;
-                    correction.b = -error.b * 0.7;
-                #endif
-                // Add compensation to original values
+                    // deuteranopia / deuteranomaly corrections (tries to mimic Android, GdH)
+                    #elif ( COLORBLIND_MODE == 2 )
+                        correction.r = error.r * -0.7; // + error.g * 0.0 + error.b * 0.0;
+                        correction.g = error.r *  0.5 + error.g; // * 1.0 + error.b * 0.0;
+                        correction.b = error.r * -0.3 + error.b; // + error.g * 0.0;
+
+                    // tritanopia / tritanomaly corrections (GdH)
+                    #elif ( COLORBLIND_MODE == 3 )
+                        correction.r = error.r * 0.3 + error.g * 0.5 + error.b * 0.4;
+                        correction.g = error.r * 0.5 + error.g * 0.7 + error.b * 0.3;
+                        correction.b = error.r * 0.0 + error.g * 0.0 + error.b * 1.0;
+                    #endif
+
+                    // Add compensation to original values
                     correction = c + correction;
                     correction.a = c.a;
                     cogl_color_out = correction.rgba;
