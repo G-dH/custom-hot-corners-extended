ad2795d57778742d78162d37d7614774c02653a9
G-dH <73719686+G-dH@users.noreply.github.com>
2025-02-07 17:53:05 +0100

    actions: Support unredirect_for_display in GNOME 48

--- src/extension/actions.js
+++ src/extension/actions.js
@@ -2114,9 +2114,15 @@ class OsdMonitorLabel extends St.Widget {
         Main.uiGroup.set_child_above_sibling(this, null);
         this._position();
 
-        Meta.disable_unredirect_for_display(global.display);
+        if (Meta.disable_unredirect_for_display)
+            Meta.disable_unredirect_for_display(global.display);
+        else // since GS 48
+            global.compositor.disable_unredirect();
         this.connect('destroy', () => {
-            Meta.enable_unredirect_for_display(global.display);
+            if (Meta.enable_unredirect_for_display)
+                Meta.enable_unredirect_for_display(global.display);
+            else // since GS 48
+                global.compositor.enable_unredirect();
         });
     }
 
